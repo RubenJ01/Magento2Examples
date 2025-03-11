0ac78d1d3c7f0bf330e564694f5c092027ec2711
Ruben Eekhof <45236226+RubenJ01@users.noreply.github.com>
2025-03-11 10:36:03 +0100

    Update GetAllCustomerGroups.php

--- GetAllCustomerGroups.php
+++ GetAllCustomerGroups.php
@@ -6,6 +6,11 @@ class GetAllCustomerGroups
 {
     private CustomerGroupCollection $customerGroup;
 
+    public function __construct(CustomerGroupCollection $customerGroupCollection)
+    {
+        $this->customerGroup = $customerGroupCollection;
+    }
+
     public function getCustomerGroups(): array
     {
         return $this->customerGroup->toOptionArray();
